ed1c3018c20337fcc8bd69598d669cd41efefd4d
EliuX <ehernandez@grubhub.com>
2020-03-31 14:28:25 -0500

    Fixes #54 Enable CORS

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="time-tracker-api",
+    name="time-tracker-backend",
     packages=find_packages(),
     include_package_data=True,
 )
