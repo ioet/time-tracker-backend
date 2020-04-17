28e4dc60f303043a9e31ba350bb507136617fdf1
roberto <alexandro.ramr777@gmail.com>
2020-04-17 15:38:09 -0500

    feat: add git tag version in wheel build

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup, find_packages
+import sys
+
+
+def get_version() -> str:
+    version = {}
+    path_to_file_with_version = "time_tracker_api/__init__.py"
+    try:
+        with open(path_to_file_with_version) as fp:
+            exec(fp.read(), version)
+        __version__ = version['__version__']
+        return __version__
+    except KeyError:
+        print(f'Error: No variable ___version___ in {path_to_file_with_version}')
+        sys.exit()
+
 
 setup(
     name="time-tracker-backend",
+    version=get_version(),
     packages=find_packages(),
     include_package_data=True,
 )
