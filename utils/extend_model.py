449623fd9bbce152ef688327bbbe2007a110e761
roberto <alexandro.ramr777@gmail.com>
2020-05-21 11:19:30 -0500

    refactor: :ok_hand: add docs to utils.extend_model.py

--- utils/extend_model.py
+++ utils/extend_model.py
@@ -1,5 +1,12 @@
-# TODO : check if we can improve this by using the overwritten __add__ method
 def add_customer_name_to_projects(projects, customers):
+    """
+    Add attribute customer_name in project model, based on customer_id of the
+    project
+    :param (list) projects: projects retrieved from project repository
+    :param (list) customers: customers retrieved from customer repository
+
+    TODO : check if we can improve this by using the overwritten __add__ method
+    """
     for project in projects:
         for customer in customers:
             if project.customer_id == customer.id:
@@ -7,6 +14,14 @@ def add_customer_name_to_projects(projects, customers):
 
 
 def add_project_name_to_time_entries(time_entries, projects):
+    """
+    Add attribute project_name in time-entry model, based on project_id of the
+    time_entry
+    :param (list) time_entries: time_entries retrieved from time-entry repository
+    :param (list) projects: projects retrieved from project repository
+
+    TODO : check if we can improve this by using the overwritten __add__ method
+    """
     for time_entry in time_entries:
         for project in projects:
             if time_entry.project_id == project.id:
