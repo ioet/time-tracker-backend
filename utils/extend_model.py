12cb1290b6ea1154c56d7cdf1ba61443d08af241
Dickson Armijos <fabidick22@hotmail.es>
2020-06-11 09:47:14 -0500

    feature: Detect if an project was deleted

--- utils/extend_model.py
+++ utils/extend_model.py
@@ -28,7 +28,8 @@ def add_project_name_to_time_entries(time_entries, projects):
     for time_entry in time_entries:
         for project in projects:
             if time_entry.project_id == project.id:
-                setattr(time_entry, 'project_name', project.name)
+                name = project.name + " (archived)" if project.is_deleted() else project.name
+                setattr(time_entry, 'project_name', name)
 
 
 def add_activity_name_to_time_entries(time_entries, activities):
