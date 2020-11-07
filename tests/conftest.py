c33c29c4a568a39634f2f816ef6c6b8827a33f29
Rene Enriquez <enriquezrene.ap@gmail.com>
2020-11-07 17:11:57 -0500

    Splitting long classes into their own separate files

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ from commons.data_access_layer.database import EventContext
 from time_tracker_api import create_app
 from time_tracker_api.database import init_sql
 from time_tracker_api.security import get_or_generate_dev_secret_key
-from time_tracker_api.time_entries.time_entries_model import (
+from time_tracker_api.time_entries.time_entries_repository import (
     TimeEntryCosmosDBRepository,
 )
 
