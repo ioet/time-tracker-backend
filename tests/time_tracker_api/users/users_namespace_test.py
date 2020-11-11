536ce77b0215de26e58b8dfe26a5f289726184de
magallegos1996 <bwcekos@gmail.com>
2020-11-11 16:31:07 -0500

    feat: users endpoint test expected response props and http status code #230

--- tests/time_tracker_api/users/users_namespace_test.py
+++ tests/time_tracker_api/users/users_namespace_test.py
@@ -1,6 +1,4 @@
-from unittest.mock import ANY, Mock, patch
-
-# from mock import patch
+from unittest.mock import ANY, Mock
 
 from faker import Faker
 from flask import json
@@ -8,30 +6,24 @@ from flask.testing import FlaskClient
 from flask_restplus._http import HTTPStatus
 from pytest_mock import MockFixture
 
+from utils.azure_users import AzureConnection
 
-@patch(
-    'utils.azure_users.AzureConnection.users',
-    new_callable=Mock(return_value=[{'name': 'dummy', 'email': 'dummy'}]),
-)
-@patch('utils.azure_users.AzureConnection', new_callable=Mock)
-@patch('msal.ConfidentialClientApplication', new_callable=Mock)
-@patch(
-    'msal.ConfidentialClientApplication.acquire_token_for_client',
-    new_callable=Mock,
-)
+
 def test_users_response_contains_expected_props(
-    acquire_token_method,
-    msal_client,
-    azure_connection,
-    users_method,
     client: FlaskClient,
     valid_header: dict,
 ):
 
-    # from time_tracker_api.users.users_namespace import azure_connection
-    # azure_connection.users = Mock(return_value=[{'name':'dummy', 'email': 'dummy'}])
+    AzureConnection.users = Mock(
+        return_value=[{'name': 'dummy', 'email': 'dummy', 'role': 'dummy'}]
+    )
 
-    response = client.get('/time-entries/users', headers=valid_header,)
+    response = client.get(
+        '/users',
+        headers=valid_header,
+    )
 
+    assert HTTPStatus.OK == response.status_code
     assert 'name' in json.loads(response.data)[0]
     assert 'email' in json.loads(response.data)[0]
+    assert 'role' in json.loads(response.data)[0]
