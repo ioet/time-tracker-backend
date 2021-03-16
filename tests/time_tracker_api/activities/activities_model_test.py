690af819a06296bddae2b0f3cf75d7102f7b82dd
roberto <alexandro.ramr777@gmail.com>
2021-03-16 17:00:35 -0500

    test: TT-180 add test find_all_with_id_in_list

--- tests/time_tracker_api/activities/activities_model_test.py
+++ tests/time_tracker_api/activities/activities_model_test.py
@@ -6,6 +6,7 @@ from typing import List
 from commons.data_access_layer.database import EventContext
 from time_tracker_api.activities.activities_model import (
     ActivityCosmosDBRepository,
+    ActivityCosmosDBModel,
 )
 
 
@@ -59,7 +60,6 @@ def test_create_sql_in_condition(
     assert expected_result == result
 
 
-""""
 @patch(
     'time_tracker_api.activities.activities_model.ActivityCosmosDBRepository.create_sql_condition_for_visibility'
 )
@@ -69,24 +69,32 @@ def test_create_sql_in_condition(
 @patch(
     'time_tracker_api.activities.activities_model.ActivityCosmosDBRepository.find_partition_key_value'
 )
-@patch(
-    'time_tracker_api.activities.activities_model.ActivityCosmosDBRepository.container'
-)
-@patch(
-    'time_tracker_api.activities.activities_model.ActivityCosmosDBRepository.get_mapper_or_dict'
-)
-def test_find_all_with_id_in_list(    
-    get_mapper_or_dict_mock,    
-    container_mock,    
-    find_partition_key_value_mock,    
+def test_find_all_with_id_in_list(
+    find_partition_key_value_mock,
     create_sql_in_condition_mock,
-    create_sql_condition_for_visibility_mock,    
+    create_sql_condition_for_visibility_mock,
     event_context: EventContext,
     activity_repository: ActivityCosmosDBRepository,
 ):
-    activity_repository.find_all_with_id_in_list(
-        event_context,
-        []
-    )
-    #query_items_mock.assert_called_once()
-"""
+    expected_item = {
+        'id': 'id1',
+        'name': 'testing',
+        'description': 'do some testing',
+        'tenant_id': 'tenantid1',
+    }
+
+    query_items_mock = Mock(return_value=[expected_item])
+    activity_repository.container = Mock()
+    activity_repository.container.query_items = query_items_mock
+
+    result = activity_repository.find_all_with_id_in_list(event_context, [])
+
+    create_sql_condition_for_visibility_mock.assert_called_once()
+    create_sql_in_condition_mock.assert_called_once()
+    find_partition_key_value_mock.assert_called_once()
+    query_items_mock.assert_called_once()
+
+    assert len(result) == 1
+    activity = result[0]
+    assert isinstance(activity, ActivityCosmosDBModel)
+    assert activity.__dict__ == expected_item
