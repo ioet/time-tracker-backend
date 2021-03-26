be6e9b17bc89fb33e78172ce47e342eb2397e45f
kelly <castrofigueroakelly@gmail.com>
2021-03-26 16:01:52 -0500

    refactor: TT-185 change the scenarios in test_add_sql_date_range_condition_should_update_where_list

--- tests/time_tracker_api/time_entries/time_entries_query_builder_test.py
+++ tests/time_tracker_api/time_entries/time_entries_query_builder_test.py
@@ -15,42 +15,16 @@ def test_TimeEntryQueryBuilder_is_subclass_CosmosDBQueryBuilder():
     )
 
 
-@pytest.mark.parametrize(
-    "start_date,end_date,expected_params",
-    [
-        (
-            "2021-03-19T05:07:00.000Z",
-            "2021-03-25T10:00:00.000Z",
-            [
-                {"name": "@start_date", "value": "2021-03-19T05:07:00.000Z"},
-                {"name": "@end_date", "value": "2021-03-25T10:00:00.000Z"},
-            ],
-        ),
-        (
-            "2021-02-10T05:08:00.000Z",
-            "2021-03-20T11:00:00.000Z",
-            [
-                {"name": "@start_date", "value": "2021-02-10T05:08:00.000Z"},
-                {"name": "@end_date", "value": "2021-03-20T11:00:00.000Z"},
-            ],
-        ),
-        (
-            "2021-01-02T05:09:00.000Z",
-            "2021-01-26T12:00:00.000Z",
-            [
-                {"name": "@start_date", "value": "2021-01-02T05:09:00.000Z"},
-                {"name": "@end_date", "value": "2021-01-26T12:00:00.000Z"},
-            ],
-        ),
-    ],
-)
-def test_add_sql_date_range_condition_should_update_where_list(
-    start_date, end_date, expected_params
-):
-    time_entry_query_builder = TimeEntryQueryBuilder().add_sql_date_range_condition(
-        (start_date, end_date)
+def test_add_sql_date_range_condition_should_update_where_list():
+    time_entry_query_builder = (
+        TimeEntryQueryBuilder().add_sql_date_range_condition(
+            ("2021-03-19T05:07:00.000Z", "2021-03-25T10:00:00.000Z")
+        )
     )
-
+    expected_params = [
+        {"name": "@start_date", "value": "2021-03-19T05:07:00.000Z"},
+        {"name": "@end_date", "value": "2021-03-25T10:00:00.000Z"},
+    ]
     assert len(time_entry_query_builder.where_conditions) == 1
     assert len(time_entry_query_builder.parameters) == len(expected_params)
     assert time_entry_query_builder.get_parameters() == expected_params
