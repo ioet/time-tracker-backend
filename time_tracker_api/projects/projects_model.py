ed3b15029a1c69c947e092941ed536792a0b68f9
Israel Pasaca <israel_pasaca@icloud.com>
2021-03-29 14:33:10 -0500

    refactor: TT-182 create method to get all projects with list of ids

--- time_tracker_api/projects/projects_model.py
+++ time_tracker_api/projects/projects_model.py
@@ -8,7 +8,7 @@ from commons.data_access_layer.cosmos_db import (
 from time_tracker_api.database import CRUDDao, APICosmosDBDao
 from typing import List, Callable
 from commons.data_access_layer.database import EventContext
-from utils.repository import convert_list_to_tuple_string, create_sql_in_condition
+from utils.repository import create_sql_in_condition
 from time_tracker_api.customers.customers_model import (
     create_dao as customers_create_dao,
 )
@@ -66,10 +66,10 @@ class ProjectCosmosDBRepository(CosmosDBRepository):
             mapper=ProjectCosmosDBModel,
         )
 
-    def find_all_with_id_in_list(
+    def find_all_v2(
         self,
         event_context: EventContext,
-        ids_list: List[str] = None,
+        ids_list: List[str],
         customers_ids_list: List[str] = None,
         visible_only=True,
         mapper: Callable = None, 
@@ -77,37 +77,10 @@ class ProjectCosmosDBRepository(CosmosDBRepository):
         query_builder = (CosmosDBQueryBuilder()
             .add_sql_in_condition("id",ids_list)
             .add_sql_in_condition("customer_id",customers_ids_list)
-            .add_sql_visibility_condition(visibility_only)
+            .add_sql_visibility_condition(visible_only)
             .build()
         )
         query_str = query_builder.get_query()
-        tenant_id_value = self.find_partition_key_value(event_context)
-
-        result = self.container.query_items(
-            query=query_str,
-            partition_key=tenant_id_value,
-        )
-        function_mapper = self.get_mapper_or_dict(mapper)
-        return list(map(function_mapper, result))
-
-
-    def find_all_with_customer_id_in_list(
-        self,
-        event_context: EventContext,
-        customer_ids_list: List[str],
-        visible_only=True,
-        mapper: Callable = None, 
-    ):  
-        visibility = self.create_sql_condition_for_visibility(visible_only)
-        query_str = """
-            SELECT * FROM c
-            WHERE {condition}
-            {visibility_condition}
-            """.format(
-            condition=create_sql_in_condition("customer_id", customer_ids_list),
-            visibility_condition=visibility,
-        )
-        
         tenant_id_value = self.find_partition_key_value(event_context)
         result = self.container.query_items(
             query=query_str,
@@ -149,9 +122,9 @@ class ProjectCosmosDBDao(APICosmosDBDao, ProjectDao):
         add_customer_name_to_projects(projects, customers)
         return projects
 
-    def get_all_with_id_in_list(self,id_list,):
+    def get_all_with_id_in_list(self,id_list):
     	event_ctx = self.create_event_context("read-many")
-    	return self.repository.find_all_with_id_in_list(event_ctx, id_list)
+    	return self.repository.find_all_v2(event_ctx, id_list)
 
 
 
