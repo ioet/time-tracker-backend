7d66d183a6f25561168b0d80968fe6229b62123f
Sandro Castillo <blosomsam@gmail.com>
2021-04-14 16:20:33 -0500

    fix: TT-84 not-allow-deleting-customers

--- time_tracker_api/customers/customers_namespace.py
+++ time_tracker_api/customers/customers_namespace.py
@@ -2,39 +2,45 @@ from faker import Faker
 from flask_restplus import Resource, fields
 from flask_restplus._http import HTTPStatus
 
-from time_tracker_api.api import common_fields, api, remove_required_constraint, NullableString
+from time_tracker_api.api import (
+    common_fields,
+    api,
+    remove_required_constraint,
+    NullableString,
+)
 from time_tracker_api.customers.customers_model import create_dao
 
 faker = Faker()
 
-ns = api.namespace('customers', description='Namespace of the API for customers')
+ns = api.namespace(
+    'customers', description='Namespace of the API for customers'
+)
 
 # Customer Model
-customer_input = ns.model('CustomerInput', {
-    'name': fields.String(
-        title='Name',
-        required=True,
-        max_length=50,
-        description='Name of the customer',
-        example=faker.company(),
-    ),
-    'description': NullableString(
-        title='Description',
-        required=False,
-        max_length=250,
-        description='Description about the customer',
-        example=faker.paragraph(),
-    ),
-})
+customer_input = ns.model(
+    'CustomerInput',
+    {
+        'name': fields.String(
+            title='Name',
+            required=True,
+            max_length=50,
+            description='Name of the customer',
+            example=faker.company(),
+        ),
+        'description': NullableString(
+            title='Description',
+            required=False,
+            max_length=250,
+            description='Description about the customer',
+            example=faker.paragraph(),
+        ),
+    },
+)
 
 customer_response_fields = {}
 customer_response_fields.update(common_fields)
 
-customer = ns.inherit(
-    'Customer',
-    customer_input,
-    customer_response_fields
-)
+customer = ns.inherit('Customer', customer_input, customer_response_fields)
 
 customer_dao = create_dao()
 
@@ -49,8 +55,10 @@ class Customers(Resource):
 
     @ns.doc('create_customer')
     @ns.response(HTTPStatus.CONFLICT, 'This customer already exists')
-    @ns.response(HTTPStatus.BAD_REQUEST, 'Invalid format or structure '
-                                         'of the attributes of the customer')
+    @ns.response(
+        HTTPStatus.BAD_REQUEST,
+        'Invalid format or structure ' 'of the attributes of the customer',
+    )
     @ns.expect(customer_input)
     @ns.marshal_with(customer, code=HTTPStatus.CREATED)
     def post(self):
@@ -64,16 +72,22 @@ class Customers(Resource):
 @ns.param('id', 'The customer identifier')
 class Customer(Resource):
     @ns.doc('get_customer')
-    @ns.response(HTTPStatus.UNPROCESSABLE_ENTITY, 'The id has an invalid format')
+    @ns.response(
+        HTTPStatus.UNPROCESSABLE_ENTITY, 'The id has an invalid format'
+    )
     @ns.marshal_with(customer)
     def get(self, id):
         """Get a customer"""
         return customer_dao.get(id)
 
     @ns.doc('update_customer')
-    @ns.response(HTTPStatus.BAD_REQUEST, 'Invalid format or structure '
-                                         'of the attributes of the customer')
-    @ns.response(HTTPStatus.CONFLICT, 'A customer already exists with this new data')
+    @ns.response(
+        HTTPStatus.BAD_REQUEST,
+        'Invalid format or structure ' 'of the attributes of the customer',
+    )
+    @ns.response(
+        HTTPStatus.CONFLICT, 'A customer already exists with this new data'
+    )
     @ns.expect(remove_required_constraint(customer_input))
     @ns.marshal_with(customer)
     def put(self, id):
@@ -84,5 +98,5 @@ class Customer(Resource):
     @ns.response(HTTPStatus.NO_CONTENT, 'Customer successfully deleted')
     def delete(self, id):
         """Delete a customer"""
-        customer_dao.delete(id)
+        customer_dao.update(id, {'delete': 'True'})
         return None, HTTPStatus.NO_CONTENT
