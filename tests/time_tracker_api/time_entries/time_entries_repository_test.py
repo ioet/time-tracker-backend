a3229869c18c9392c619cbde7cd73f550d3c8dbd
Kevin Lopez <kevinjeff04@gmail.com>
2021-06-14 16:36:58 -0500

    fix: TT-261 refactor code based on PR reviews

--- tests/time_tracker_api/time_entries/time_entries_repository_test.py
+++ tests/time_tracker_api/time_entries/time_entries_repository_test.py
@@ -13,42 +13,33 @@ from time_tracker_api.activities.activities_model import (
     ActivityCosmosDBModel,
 )
 
+time_entry_data = {
+    'id': 'id',
+    'start_date': '2021-03-22T10:00:00.000Z',
+    'end_date': "2021-03-22T11:00:00.000Z",
+    'description': 'do some testing',
+    'tenant_id': 'tenant_id',
+    'project_id': 'project_id1',
+    'activity_id': 'activity_id1',
+    'technologies': ['python', 'pytest'],
+    'owner_id': 'id',
+}
 
-def create_time_entry_data():
-    time_entry_data = {
-        'id': 'id',
-        'start_date': '2021-03-22T10:00:00.000Z',
-        'end_date': "2021-03-22T11:00:00.000Z",
-        'description': 'do some testing',
-        'tenant_id': 'tenant_id',
-        'project_id': 'project_id1',
-        'activity_id': 'activity_id1',
-        'technologies': ['python', 'pytest'],
-        'owner_id': 'id',
-    }
-    return time_entry_data
+project_data = {
+    'customer_id': 'dsakldh12ASD',
+    'id': 'project_id1',
+    'name': 'project_name',
+    'description': 'do some testing',
+    'project_type_id': "id2",
+    'tenant_id': 'tenantid1',
+}
 
-
-def create_project_data():
-    project_data = {
-        'customer_id': 'dsakldh12ASD',
-        'id': 'project_id1',
-        'name': 'project_name',
-        'description': 'do some testing',
-        'project_type_id': "id2",
-        'tenant_id': 'tenantid1',
-    }
-    return project_data
-
-
-def create_activity_data():
-    activity_data = {
-        'id': 'activity_id1',
-        'name': 'activity',
-        'description': 'testing',
-        "tenant_id": 'nomatter',
-    }
-    return activity_data
+activity_data = {
+    'id': 'activity_id1',
+    'name': 'activity',
+    'description': 'testing',
+    "tenant_id": 'nomatter',
+}
 
 
 def test_add_complementary_info(
@@ -63,15 +54,15 @@ def test_add_complementary_info(
     )
     users_mock = mocker.patch.object(AzureConnection, 'users')
 
-    expected_project = ProjectCosmosDBModel(create_project_data())
-    expected_activity = ActivityCosmosDBModel(create_activity_data())
-    expected_time_entry_in = TimeEntryCosmosDBModel(create_time_entry_data())
-    expected_users = AzureUser('email1', [], 'id', 'name', ['admin'])
+    expected_project = ProjectCosmosDBModel(project_data)
+    expected_activity = ActivityCosmosDBModel(activity_data)
+    expected_time_entry_in = TimeEntryCosmosDBModel(time_entry_data)
+    expected_user = AzureUser('email1', [], 'id', 'name', ['admin'])
     setattr(expected_project, 'customer_name', 'customer_name')
 
     projects_db_get_all_mock.return_value = [expected_project]
     activities_db_get_all_mock.return_value = [expected_activity]
-    users_mock.return_value = [expected_users]
+    users_mock.return_value = [expected_user]
 
     expected_time_entry_out = time_entry_repository.add_complementary_info(
         [expected_time_entry_in], max_count=None, exist_conditions=True
