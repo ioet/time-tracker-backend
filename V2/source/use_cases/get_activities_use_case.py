a936f0a58fc3c87d05da20f4caff823206b657f7
JosueOb <josuericardocando@hotmail.com>
2021-09-30 11:38:54 -0500

    refactor: TT-352 use list comprehensions

--- V2/source/use_cases/get_activities_use_case.py
+++ V2/source/use_cases/get_activities_use_case.py
@@ -4,23 +4,7 @@ from V2.source.daos.activities_json_dao import ActivitiesJsonDao
 def get_list_activities():
     activity_json_dao = ActivitiesJsonDao('./V2/source/activities_data.json')
     activity_service = ActivityService(activity_json_dao)
-    list_activities_dto = activity_service.get_all()
-    #print(list_activities_dto)
+    activities_dto = activity_service.get_all()
+    activities = [activity_dto.__dict__ for activity_dto in activities_dto]
 
-    list_activities_json = []
-
-    for activity_dto in list_activities_dto:
-        activity_json = {
-            'id': activity_dto.id,
-            'name': activity_dto.name,
-            'description': activity_dto.description,
-            'deleted': activity_dto.deleted,
-            'status': activity_dto.status,
-            'tenant_id': activity_dto.tenant_id,
-
-        }
-        list_activities_json.append(activity_json)
-
-    #print(list_activities_json)
-
-    return list_activities_json
+    return activities
--- V2/source/use_cases/get_activity_by_id_use_case.py
+++ V2/source/use_cases/get_activity_by_id_use_case.py
@@ -5,17 +5,6 @@ def get_activity_by_id(id: str):
     activity_json_dao = ActivitiesJsonDao('./V2/source/activities_data.json')
     activity_service = ActivityService(activity_json_dao)
     activity_dto = activity_service.get_by_id(id)
-    #print(activity_dto)
+    activity = activity_dto.__dict__
 
-    activity_json = {
-        'id': activity_dto.id,
-        'name': activity_dto.name,
-        'description': activity_dto.description,
-        'deleted': activity_dto.deleted,
-        'status': activity_dto.status,
-        'tenant_id': activity_dto.tenant_id,
-
-    }
-    #print(activity_json)
-
-    return activity_json
+    return activity
