9711cebc3792b633ec920c848564c8b8d6bdda1c
Andrés Soto <edansrock@pop-os.localdomain>
2021-10-11 17:30:11 -0500

    feat: TT-356 Read activities with an azure endpoint

---
+++ V2/azure_time_tracker/activities/__init__.py
@@ -0,0 +1,20 @@
+from time_tracker.source.use_cases.use_case import get_all, get_by_id
+
+import logging
+import json
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info(
+        'Python HTTP trigger function processed a request to get an activity.'
+    )
+    mimetype = "application/json"
+    id = req.route_params.get('id')
+
+    if id:
+        response = json.dumps(get_by_id(id))
+    else:
+        response = json.dumps(get_all())
+
+    return func.HttpResponse(body=response, status_code=200, mimetype=mimetype)
