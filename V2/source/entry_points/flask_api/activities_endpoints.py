fd84ea15566b8fd3e0a438e5bfd136e8137d7240
JosueOb <josuericardocando@hotmail.com>
2021-10-13 11:42:59 -0500

    refactor: TT-352 refactoring of activity endpoints

--- V2/source/entry_points/flask_api/activities_endpoints.py
+++ V2/source/entry_points/flask_api/activities_endpoints.py
@@ -9,18 +9,23 @@ JSON_PATH = './V2/source/activities_data.json'
 
 class Activities(Resource):
     def get(self):
-        activity_json = ActivitiesJsonDao(JSON_PATH)
-        activity_service = ActivityService(activity_json)
-        activities = use_cases.GetActivitiesUseCase(activity_service)
+        activities = use_cases.GetActivitiesUseCase(
+            create_activity_service(JSON_PATH)
+        )
         return [activity.__dict__ for activity in activities.get_activities()]
 
 
 class Activity(Resource):
     def get(self, activity_id: str):
         try:
-            activity_json = ActivitiesJsonDao(JSON_PATH)
-            activity_service = ActivityService(activity_json)
-            activity = use_cases.GetActivityUseCase(activity_service)
+            activity = use_cases.GetActivityUseCase(
+                create_activity_service(JSON_PATH)
+            )
             return activity.get_activity_by_id(activity_id).__dict__
         except AttributeError:
             return {'message': 'Activity not found'}, HTTPStatus.NOT_FOUND
+
+
+def create_activity_service(path: str):
+    activity_json = ActivitiesJsonDao(path)
+    return ActivityService(activity_json)
