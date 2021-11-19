c04ec5a69ac97533d319d6569d124a507bc20e97
Jipson Murillo <jipsonmurillo@gmail.com>
2021-11-19 12:27:01 -0500

    refactor: TT-402 rebase post time entry

--- V2/time_tracker/time_entries/_application/_time_entries/_update_time_entry.py
+++ V2/time_tracker/time_entries/_application/_time_entries/_update_time_entry.py
@@ -9,7 +9,6 @@ from time_tracker.time_entries._domain import TimeEntryService, TimeEntry, _use_
 from time_tracker._infrastructure import DB
 
 
-
 def update_time_entry(req: func.HttpRequest) -> func.HttpResponse:
     logging.info(
         'Python HTTP trigger function processed a request to update an time entry.'
