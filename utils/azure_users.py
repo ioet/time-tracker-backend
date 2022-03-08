bed2cc14e781c5544806da1084efcedb2ad84411
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2022-03-08 14:17:57 -0500

    Fix tests and show all ioet users

--- utils/azure_users.py
+++ utils/azure_users.py
@@ -133,9 +133,10 @@ class AzureConnection:
                 )[1]
                 endpoint = endpoint + skip_token_attribute + request_token
                 
-        for i in range(len(users)):
-             if users[i]['mail'] is None:
-                 valid_users.append(users[i])
+        for user in users:
+            user_emails = user['otherMails']
+            if(len(user_emails) != 0 and user_emails[0].split('@')[1] == 'ioet.com'):
+                valid_users.append(user)
         
         return [self.to_azure_user(user) for user in valid_users]
 
